de67ff705fc9200f3c5b16669ea05eb0184ae69f
hg8lhs <hg8lhs@4e581012-1572-46c6-82a8-383d2fed1511>
2008-11-23 13:46:02 +0000

    Added ADC support.
    git-svn-id: file:///tmp/Diploma/trunk@74 4e581012-1572-46c6-82a8-383d2fed1511

---
+++ Firmware/stm32/adc.c
@@ -0,0 +1,72 @@
+// ---------------------------------------------------------------------------
+// This header file contains the function prototypes for the Analog-Digital
+// Converter.
+// ---------------------------------------------------------------------------
+
+#include <adc.h>
+#include <stm32f10x_adc.h>
+#include <stm32f10x_rcc.h>
+
+// ---------------------------------------------------------------------------
+// This function initializes the A/D converter.
+// ---------------------------------------------------------------------------
+
+void adc_init() {
+    GPIO_InitTypeDef GPIO_InitStructure;
+    ADC_InitTypeDef ADC_InitStructure;
+    
+    /* Enable ADC1, ADC2 and GPIOC clock */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_ADC2, ENABLE);
+    /* ADCCLK = PCLK2/4 */
+    RCC_ADCCLKConfig(RCC_PCLK2_Div4);
+
+    /* Configure PC.01 and PC.02 (ADC Channel11 and 12) as analog input */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    /* ADC1 configuration --------------------------------------------------*/
+    ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+    ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+    ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+    ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+    ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+    ADC_InitStructure.ADC_NbrOfChannel = 2;
+    ADC_Init(ADC1, &ADC_InitStructure);
+
+    /* ADC1 regular channels configuration */
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_11, 1, ADC_SampleTime_239Cycles5);
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_12, 2, ADC_SampleTime_239Cycles5);
+
+    /* Enable ADC1 */
+    ADC_Cmd(ADC1, ENABLE);
+
+    /* Enable ADC1 reset calibaration register */
+    ADC_ResetCalibration(ADC1);
+
+    /* Check the end of ADC1 reset calibration register */
+    while(ADC_GetResetCalibrationStatus(ADC1));
+
+    /* Start ADC1 calibaration */
+    ADC_StartCalibration(ADC1);
+
+    /* Check the end of ADC1 calibration */
+    while(ADC_GetCalibrationStatus(ADC1));
+}
+
+// -------------------------------------------------------------------------
+// Starts A/D conversion.
+// -------------------------------------------------------------------------
+
+unsigned int adc_convert() {
+    /* Start ADC1 Software Conversion */
+    ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+
+    /* Check the end of ADC1 calibration */
+    while(ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC));
+    
+    ADC_ClearFlag(ADC1, ADC_FLAG_EOC);
+
+    return ADC_GetConversionValue(ADC1) & 4095;
+}
+
